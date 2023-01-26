43bf86d48252256ae8d6a1ca33b89e51d65d746b
StuartCowley <voidstalker@live.co.uk>
2023-01-26 15:38:57 +0000

    Add internal links to news page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 // domain.com/news
+import Link from "next/link";
 
 const NewsPage = () => {
-  return <div>News page</div>;
+  return (
+    <>
+      <div>News page</div>
+      <ul>
+        <li>
+          <Link href="/news/something-useful">something useful</Link>
+        </li>
+        <li>
+          <Link href="/news/a-second-link">a second link</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
