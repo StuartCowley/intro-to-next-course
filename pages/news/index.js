a32ee2176abf48d17fe659a49246fc61e94bfea0
StuartCowley <voidstalker@live.co.uk>
2023-01-26 15:16:12 +0000

    Strip back boilerplate, create basic subpages layout

---
+++ pages/news/index.js
@@ -0,0 +1,7 @@
+// domain.com/news
+
+const NewsPage = () => {
+  return <div>News page</div>;
+};
+
+export default NewsPage;
---
+++ pages/news/something-important/index.js
@@ -0,0 +1,7 @@
+// domain.com/news/something-important (from parent folder name)
+
+const NewsPage = () => {
+  return <div>Something important</div>;
+};
+
+export default NewsPage;
